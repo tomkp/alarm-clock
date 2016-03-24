a0e50eee2f01117a2a713063acf3aed8c398fd5b
Tom Kemp-Potter <tom.kemp-potter@db.com>
2016-03-24 14:59:19 +0000

    ...

---
+++ src/App.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const App = React.createClass({
+    render: function () {
+        return <p>Hello World</p>;
+    }
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));

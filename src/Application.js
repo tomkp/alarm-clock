aaf54f4f57ba22d53720c7f66f9065df3ba83a07
tomkp <tom@tomkp.com>
2016-04-02 09:11:06 +0100

    ...

--- src/Application.js
+++ src/Application.js
@@ -10,32 +10,50 @@ class Timer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            time: moment(),
-            blink: false
+            time: moment()
         };
     }
     tick() {
         this.setState({
-            time: moment(),
-            //blink: !this.state.blink
+            //time: moment('19:15', 'HH:mm')
+            time: moment()
         });
     }
     componentDidMount() {
         setInterval(() => { this.tick() }, 500); // Call a method on the mixin
     }
     render() {
-        const hour = Number(this.state.time.format('HH'));
-        const isMorning = (hour >= 6 && hour < 7);
-        const isDay = (hour >= 7 && hour <= 19);
-        const isEvening = (hour > 19 && hour < 20);
-        return (
-        <div className={'timer ' + (isMorning?'morning':'') + (isDay?'day':'night') + (isEvening?'evening':'')}>
-                <time className={(this.state.blink?'on':'off')}>{this.state.time.format('h')}</time>
-                <Progress minute={this.state.time.format('m')}/>
-            </div>
-        );
+
+        return <Display time={this.state.time}/>
     }
 }
+
+const Display = ({time}) => {
+
+    const hour24 = Number(time.format('HH'));
+    const hour12 = Number(time.format('h'));
+    const minute = time.format('m');
+
+    const isMorning = (hour24 >= 6 && hour24 < 7);
+    const isDay = (hour24 >= 7 && hour24 < 19);
+    const isEvening = (hour24 >= 19 && hour24 < 20);
+
+    const timeOfDay =
+        (isMorning ? 'morning ' :
+            (isDay ? 'day ' :
+                (isEvening ? 'evening ' : 'night ')));
+
+    //console.log(`${hour24} is ${timeOfDay}`);
+
+    return (
+        <div className={'timer ' + timeOfDay}>
+            <time>{hour12}</time>
+            <Progress minute={minute}/>
+        </div>
+    );
+};
+
+
 const Progress = ({minute}) => {
     const width = (Math.round(100 * minute / 60 ) + '%');
     return <div className="progress" style={{width: width}}></div>
